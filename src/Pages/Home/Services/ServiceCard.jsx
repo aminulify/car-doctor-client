3708e3ca7173db43cc38b960cc66584a910d761a
mdaminul1024 <mdfahad1024@gmail.com>
2024-04-02 15:18:13 +0600

    service card made full button

--- src/Pages/Home/Services/ServiceCard.jsx
+++ src/Pages/Home/Services/ServiceCard.jsx
@@ -3,15 +3,18 @@ import { Link } from "react-router-dom";
 
 const ServiceCard = ({service,handleScroll}) => {
     const {_id, img, title, price} = service;
+    
     return (
-        <div className="zoom-img p-5 border rounded-md hover:border-[var(--mainColor)] hover:duration-500">
+        <Link onClick={handleScroll} to={`/services/${_id}`}>
+            <div className="zoom-img p-5 border rounded-md hover:border-[var(--mainColor)] hover:duration-500">
             <div className="image-hover-zoom"><img src={img} className="w-full h-[200px] object-cover" alt="service_image" /></div>
             <h1 className="text-left mt-2 text-xl font-bold">{title}</h1>
             <div className="flex justify-between items-center">
                 <p className="font-bold text-[var(--mainColor)] py-1">Price: ${price}</p>
-                <Link onClick={handleScroll} to={`/services/${_id}`}><div className="text-2xl text-[var(--mainColor)] hover:text-slate-800 hover:duration-500"><IoIosArrowDropright/></div></Link>
+                <Link><div className="text-2xl text-[var(--mainColor)] hover:text-slate-800 hover:duration-500"><IoIosArrowDropright/></div></Link>
             </div>
         </div>
+        </Link>
     );
 };
 
